05385c93cc68264bbfaaea66ed9aa1a5dde3a5b9
Dev Mandal <devmandalgps@gmail.com>
2026-06-11 22:25:40 +0530

    Updated Script.js and added math.js(MODULE>EXPORTS)

--- Script.js
+++ Script.js
@@ -1,4 +1,14 @@
-let args = process.argv;
-for (let i = 2; i < args.length; i++) {
-    console.log("Hello, " + process.argv[i] + "!");
-}
+// let args = process.argv;
+// for (let i = 2; i < args.length; i++) {
+//     console.log("Hello, " + process.argv[i] + "!");
+// }
+
+
+//MODULES.EXPORTS
+const someValue = require('./math');
+
+console.log(someValue.sum(90, 10));
+console.log(someValue.multiply(90, 10));
+console.log(someValue.g);
+console.log(someValue.pi);
+
---
+++ math.js
@@ -0,0 +1,12 @@
+//MODULES.EXPORTS
+
+const sum = (a, b) => a + b;
+const multiply = (a, b) => a * b;
+const g = 9.8;
+const pi = 3.14;
+module.exports = { sum, multiply, g, pi};
+
+// module.exports.sum = (a, b) => a + b;
+// module.exports.multiply = (a, b) => a * b;
+// module.exports.g = 9.8
+// module.exports.pi = 3.14;
